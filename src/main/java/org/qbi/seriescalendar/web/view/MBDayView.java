0569dc94272384b9600a0a7c21172f631d3935e3
Porció László <Porci Lszl@192.168.10.104>
2014-10-20 12:30:39 +0200

    Merge origin/master

--- src/main/java/org/qbi/seriescalendar/web/view/MBDayView.java
+++ src/main/java/org/qbi/seriescalendar/web/view/MBDayView.java
@@ -9,8 +9,12 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.component.panel.Panel;
+import org.primefaces.event.DragDropEvent;
 import org.qbi.seriescalendar.web.model.Day;
 
 /**
@@ -28,6 +32,17 @@ public class MBDayView implements Serializable {
 
         daysOfWeek = new ArrayList<>();
 
+    }
+
+    public void onDragNDrop(DragDropEvent event) {
+
+        Panel panel = (Panel) event.getComponent().findComponent(event.getDragId());
+        String day = panel.getAttributes().get("seriesDay").toString();
+        String title = panel.getAttributes().get("seriesTitle").toString();
+
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO,
+                "Day: " + day + "<br/>" + "Title:" + title, null);
+        FacesContext.getCurrentInstance().addMessage(null, msg);
     }
 
     public List<Day> getDaysOfWeek() {
