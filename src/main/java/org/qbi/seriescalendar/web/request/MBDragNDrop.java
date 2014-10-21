4c747d5ca12e540a1758bb0786ec47ffd9d6d20d
Porció László <Porci Lszl@192.168.10.104>
2014-10-21 12:15:05 +0200

    Merge origin/master

--- src/main/java/org/qbi/seriescalendar/web/request/MBDragNDrop.java
+++ src/main/java/org/qbi/seriescalendar/web/request/MBDragNDrop.java
@@ -9,6 +9,7 @@ import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+import org.apache.log4j.Logger;
 import org.primefaces.component.panel.Panel;
 import org.primefaces.event.DragDropEvent;
 
@@ -20,17 +21,22 @@ import org.primefaces.event.DragDropEvent;
 @RequestScoped
 public class MBDragNDrop {
 
+    final static Logger logger = Logger.getLogger(MBDragNDrop.class);
+    
     public void onDragNDrop(DragDropEvent event) {
 
         System.out.println("drop");
         System.out.println(event.getDragId());
-        Panel panel = (Panel) event.getComponent().findComponent(event.getDragId());
-        String day = panel.getAttributes().get("seriesDay").toString();
-        String title = panel.getAttributes().get("seriesTitle").toString();
-        System.out.println(day);
-        System.out.println(title);
-        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO,
-                "Day: " + day + "<br/>" + "Title:" + title, null);
-        FacesContext.getCurrentInstance().addMessage(null, msg);
+        System.err.println(event.getData());
+//        Panel panel = (Panel) event.getComponent().findComponent(event.getDragId());
+//        logger.debug(panel);
+//        String day = panel.getAttributes().get("seriesDay").toString();
+//        String title = panel.getAttributes().get("seriesTitle").toString();
+//        System.out.println(day);
+//        System.out.println(title);
+//        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO,
+//                "Day: " + day + "<br/>" + "Title:" + title, null);
+//        FacesContext.getCurrentInstance().addMessage(null, msg);
+        
     }
 }
