0d42c7bd00b4f132fb46f9d62a2c51b0c6a5afce
Porció László <Porci Lszl@192.168.10.104>
2014-11-07 14:15:47 +0100

    Merge origin/master

---
+++ src/main/java/org/qbi/seriescalendar/web/request/MBUserLogin.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.qbi.seriescalendar.web.request;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import org.apache.log4j.Logger;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.primefaces.context.RequestContext;
+import org.qbi.seriescalendar.web.model.db.User;
+import org.qbi.seriescalendar.web.session.MBUser;
+import org.qbi.seriescalendar.web.utils.HibernateUtil;
+
+/**
+ *
+ * @author Qbi
+ */
+@ManagedBean(name = "mBUserLogin")
+@RequestScoped
+public class MBUserLogin {
+
+    @ManagedProperty("#{mBUser}")
+    private MBUser mBUser;
+
+    final static Logger logger = Logger.getLogger(MBUserLogin.class);
+
+    public void login(ActionEvent event) {
+
+        RequestContext context = RequestContext.getCurrentInstance();
+        FacesMessage message = null;
+
+        User user = findUserByLoginData(mBUser.getUsername(), mBUser.getPassword());
+        mBUser.setUser(user);
+
+        if (user != null) {
+            mBUser.setLoggedIn(true);
+            message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", mBUser.getUser().getFullname());
+            FacesContext.getCurrentInstance().addMessage(null, message);
+            context.addCallbackParam("loggedIn", mBUser.isLoggedIn());
+            context.update("weekForm:weekData");
+        } else {
+            mBUser.setLoggedIn(false);
+            message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Loggin Error", "Invalid credentials");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+            context.addCallbackParam("loggedIn", mBUser.isLoggedIn());
+        }
+
+    }
+
+    private User findUserById(int id) {
+        User u = null;
+
+        if (id > 0) {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            Transaction tx = session.beginTransaction();
+            String hql = "from User u where u.id = :id";
+            Query query = session.createQuery(hql);
+            query.setParameter("id", id);
+            u = (User) query.uniqueResult();
+            tx.commit();
+            session.close();
+        }
+
+        return u;
+    }
+
+    private User findUserByLoginData(String un, String pw) {
+        User u = null;
+
+        if (un != null && pw != null) {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            Transaction tx = session.beginTransaction();
+            String hql = "from User u where u.username = :username and u.password = :password";
+            Query query = session.createQuery(hql);
+            query.setParameter("username", un);
+            query.setParameter("password", pw);
+            u = (User) query.uniqueResult();
+            tx.commit();
+            session.close();
+        }
+
+        return u;
+    }
+
+    public void logout() {
+
+        RequestContext context = RequestContext.getCurrentInstance();
+        FacesMessage message = null;
+
+        mBUser.setLoggedIn(false);
+        message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Bye bye", mBUser.getUser().getFullname());
+        FacesContext.getCurrentInstance().addMessage(null, message);
+        context.update("weekForm:weekData");
+        mBUser.setUser(null);
+    }
+
+    public MBUser getmBUser() {
+        return mBUser;
+    }
+
+    public void setmBUser(MBUser mBUser) {
+        this.mBUser = mBUser;
+    }
+
+}
--- src/main/java/org/qbi/seriescalendar/web/session/MBUser.java
+++ src/main/java/org/qbi/seriescalendar/web/session/MBUser.java
@@ -7,15 +7,8 @@ package org.qbi.seriescalendar.web.session;
 
 import java.io.Serializable;
 import javax.annotation.PostConstruct;
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
-import javax.faces.event.ActionEvent;
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.primefaces.context.RequestContext;
 import org.qbi.seriescalendar.web.model.db.User;
 import org.qbi.seriescalendar.web.utils.HibernateUtil;
 
@@ -37,75 +30,6 @@ public class MBUser implements Serializable {
 
         System.out.println("init mBUser");
         HibernateUtil.getSessionFactory();
-    }
-
-    public void login(ActionEvent event) {
-
-        RequestContext context = RequestContext.getCurrentInstance();
-        FacesMessage message = null;
-
-        user = findUserByLoginData(username, password);
-
-        if (user != null) {
-            loggedIn = true;
-            message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", user.getFullname());
-            FacesContext.getCurrentInstance().addMessage(null, message);
-            context.addCallbackParam("loggedIn", loggedIn);
-            context.update("weekForm:weekData");
-        } else {
-            loggedIn = false;
-            message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Loggin Error", "Invalid credentials");
-            FacesContext.getCurrentInstance().addMessage(null, message);
-            context.addCallbackParam("loggedIn", loggedIn);
-        }
-
-    }
-
-    private User findUserById(int id) {
-        User u = null;
-
-        if (id > 0) {
-            Session session = HibernateUtil.getSessionFactory().openSession();
-            Transaction tx = session.beginTransaction();
-            String hql = "from User u where u.id = :id";
-            Query query = session.createQuery(hql);
-            query.setParameter("id", id);
-            u = (User) query.uniqueResult();
-            tx.commit();
-            session.close();
-        }
-
-        return u;
-    }
-
-    private User findUserByLoginData(String un, String pw) {
-        User u = null;
-
-        if (un != null && pw != null) {
-            Session session = HibernateUtil.getSessionFactory().openSession();
-            Transaction tx = session.beginTransaction();
-            String hql = "from User u where u.username = :username and u.password = :password";
-            Query query = session.createQuery(hql);
-            query.setParameter("username", un);
-            query.setParameter("password", pw);
-            u = (User) query.uniqueResult();
-            tx.commit();
-            session.close();
-        }
-
-        return u;
-    }
-
-    public void logout() {
-
-        RequestContext context = RequestContext.getCurrentInstance();
-        FacesMessage message = null;
-
-        loggedIn = false;
-        message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Bye bye", user.getFullname());
-        FacesContext.getCurrentInstance().addMessage(null, message);
-        context.update("weekForm:weekData");
-
     }
 
     public String getUsername() {
